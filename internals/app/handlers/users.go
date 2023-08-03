8226e9fe3244e065830c65382af9d49026c2d605
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-08-04 04:17:32 +0500

    Replace type processor to inteface

--- internals/app/handlers/users.go
+++ internals/app/handlers/users.go
@@ -6,49 +6,17 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/gorilla/mux"
-
 	"github.com/alexeyval/test-task-makves-group/internals/app/processors"
 )
 
 type UsersHandler struct {
-	processor *processors.UsersProcessor
+	processor processors.UserService
 }
 
-func NewUsersHandler(processor *processors.UsersProcessor) *UsersHandler {
+func NewUsersHandler(processor processors.UserService) *UsersHandler {
 	return &UsersHandler{
 		processor: processor,
 	}
-}
-
-func (handler *UsersHandler) Find(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	if vars["id"] == "" {
-		WrapError(w, errors.New("missing id"))
-
-		return
-	}
-
-	id, err := strconv.ParseInt(vars["id"], 10, 64)
-	if err != nil {
-		WrapError(w, err)
-
-		return
-	}
-
-	user, err := handler.processor.FindUser(id)
-	if err != nil {
-		WrapError(w, err)
-
-		return
-	}
-
-	m := map[string]interface{}{
-		"result": "OK",
-		"data":   user,
-	}
-
-	WrapOK(w, m)
 }
 
 func (handler *UsersHandler) List(w http.ResponseWriter, r *http.Request) {
